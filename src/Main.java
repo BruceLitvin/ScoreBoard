aecaefba1b1946d649be17e82440e2758a5a00e6
BruceLitvin <brucel15@nycstudents.net>
2025-11-14 21:35:43 -0500

    skeleton

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main{
+    public static void main(String[] args) throws FileNotFoundException {
+
+    }
+
+    public static int read() throws FileNotFoundException{
+        int score = 0; //start at zero
+        File f = new File("ScoreBoard.txt");
+        Scanner s = new Scanner(f);
+        return 0;
+    }
+
+
+
+}
