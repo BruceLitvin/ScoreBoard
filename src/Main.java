1f88e53a8ee0e82a9ed153057ee760549959aabe
BruceLitvin <brucel15@nycstudents.net>
2025-11-16 20:54:25 -0500

    V2

--- src/Main.java
+++ src/Main.java
@@ -11,63 +11,69 @@ public class Main {
     }
 
     public static int read() throws FileNotFoundException {
-        boolean flag = true;
-        int RTotal = 0;
+        boolean flag = true; //determines which team to add numbers to
+        int RTotal = 0;//initialize variables
         int OTotal = 0;
         int YTotal = 0;
         int GTotal = 0;
         int BTotal = 0;
         int ITotal = 0;
         int VTotal = 0;
-        File f = new File("ScoreBoard.txt");
-        Scanner s = new Scanner(f);
-        ScoreBoard[] lines = new ScoreBoard[1000];
+        File f = new File("ScoreBoard.txt");//document
+        Scanner s = new Scanner(f);//scan the document
+        ScoreBoard[] lines = new ScoreBoard[1000];//initialize the array of 1000 games
 
-        while (s.hasNext()) {
+        while (s.hasNext()) { //go through the list of strings
             String word = s.next();
             String word2 = s.next();
-            while (s.hasNextInt()) {
+            while (s.hasNextInt()) { //now track the integers and add them up
                 int num = s.nextInt();
-                if(num==0) flag=!flag;
-                else if (flag==true)
+                if(num==0) flag=!flag;// change teams when the number is zero
+                else if (flag==true) //if true add to word 1 for any of the teams
 
-                    if (word.equals("Red")) RTotal += num;
-                else if (word.equals("Orange"))
-                    OTotal = OTotal+num;
-                else if (word.equals("Yellow"))
-                    YTotal += num;
-                else if (word.equals("Green"))
-                    GTotal += num;
-                else if (word.equals("Blue"))
-                    BTotal += num;
-                else if (word.equals("Indigo"))
-                    ITotal += num;
-                else if (word.equals("Violet"))
-                    VTotal += num;
-                if (flag== false) {
                     if (word.equals("Red"))
-                        RTotal += num;
+                    { RTotal += num;}
+                else if (word.equals("Orange"))
+                    {OTotal = OTotal+num;}
+                else if (word.equals("Yellow"))
+                    {YTotal += num;}
+                else if (word.equals("Green"))
+                    { GTotal += num;}
+                else if (word.equals("Blue"))
+                    {BTotal += num;}
+                else if (word.equals("Indigo"))
+                    {ITotal += num;}
+                else if (word.equals("Violet"))
+                    {VTotal += num;}
+                if (flag== false) { //otherwise add to team 2
+                    if (word.equals("Red"))
+                    { RTotal += num;}
                     else if (word2.equals("Orange"))
-                        OTotal += num;
+                    {OTotal += num;}
                     else if (word2.equals("Yellow"))
-                        YTotal = YTotal+num;
+                    {   YTotal = YTotal+num;}
                     else if (word2.equals("Green"))
-                        GTotal += num;
+                    {  GTotal += num;}
                     else if (word2.equals("Blue"))
-                        BTotal += num;
+                    {   BTotal += num;}
                     else if (word2.equals("Indigo"))
-                        ITotal += num;
+                    {   ITotal += num;}
                     else if (word2.equals("Violet"))
-                        VTotal += num;
+                    {   VTotal += num;}
                 }
 
             }
 
 
         }
-
+        System.out.println(RTotal); //print out how many points each team gets in the end
+        System.out.println(OTotal);
+        System.out.println(YTotal);
+        System.out.println(GTotal);
+        System.out.println(BTotal);
+        System.out.println(ITotal);
+        System.out.println(VTotal);
         return 0;
-
 
     }
 }
